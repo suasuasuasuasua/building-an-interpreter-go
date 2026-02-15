a5a7cf10bdcf6742c44142d35d62609af5fd22d6
Justin Hoang <justinhoang@sua.dev>
2026-02-15 14:27:00 -0700

    feat(lexer): add more keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -18,6 +18,12 @@ func TestNextToken(t *testing.T) {
 	let result = add(five, ten);
 	!-/*5;
 	5 < 10 > 5;
+
+	if (5 < 10) {
+		return true;
+	} else {
+		return false;
+	}
 	`
 
 	tests := []struct {
@@ -72,6 +78,23 @@ func TestNextToken(t *testing.T) {
 		{token.GT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 
@@ -79,7 +102,6 @@ func TestNextToken(t *testing.T) {
 
 	for i, tt := range tests {
 		tok := l.NextToken()
-		println("%q %q", tok.Literal, tok.Type)
 
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests [%d] = tokenType wrong, expected=%q, got=%q", i, tt.expectedType, tok.Type)
