118164f444141eab495289b7c91258e0e1c907b9
Justin Hoang <justinhoang@sua.dev>
2026-03-14 17:13:50 -0600

    chore: run some goland suggestions
    Signed-off-by: Justin Hoang <justinhoang@sua.dev>

--- repl/repl.go
+++ repl/repl.go
@@ -17,7 +17,9 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Fprintf(out, PROMPT)
+		if _, err := fmt.Fprintf(out, PROMPT); err != nil {
+			return
+		}
 		scanned := scanner.Scan()
 		if !scanned {
 			return
@@ -29,7 +31,9 @@ func Start(in io.Reader, out io.Writer) {
 
 		// loop over the tokens until we encounter an EOF
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+			if _, err := fmt.Fprintf(out, "%+v\n", tok); err != nil {
+				return
+			}
 		}
 	}
 }
