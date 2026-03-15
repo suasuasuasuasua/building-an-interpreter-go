fa4a3c05f64891b84f992547cf414fe8aa9919ed
Justin Hoang <justinhoang@sua.dev>
2026-03-15 17:03:13 -0600

    feat: add if-expressions
    Signed-off-by: Justin Hoang <justinhoang@sua.dev>

--- parser/parser.go
+++ parser/parser.go
@@ -62,6 +62,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+	p.registerPrefix(token.IF, p.parseIfExpression)
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
 	p.registerInfix(token.MINUS, p.parseInfixExpression)
@@ -232,6 +233,70 @@ func (p *Parser) parseGroupedExpression() ast.Expression {
 	}
 
 	return exp
+}
+
+func (p *Parser) parseIfExpression() ast.Expression {
+	expression := &ast.IfExpression{Token: p.curToken}
+
+	// if (...) { ... } else { ... }
+	//    ^ this
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	p.nextToken()
+	expression.Condition = p.parseExpression(LOWEST)
+
+	// if (...) { ... } else { ... }
+	//        ^ this
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	// if (...) { ... } else { ... }
+	//          ^ this
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	expression.Consequence = p.parseBlockStatement()
+
+	// the else is optional
+	// if (...) { ... } else { ... }
+	//                  ^
+	if p.peekTokenIs(token.ELSE) {
+		p.nextToken()
+
+		// if (...) { ... } else { ... }
+		//                       ^
+		if !p.expectPeek(token.LBRACE) {
+			return nil
+		}
+
+		expression.Alternative = p.parseBlockStatement()
+	}
+
+	return expression
+}
+
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+
+	// advance into the block
+	p.nextToken()
+
+	// add statements while we haven't seen the closing brace or end of file
+	// marker
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return block
 }
 
 func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
